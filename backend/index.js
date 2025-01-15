ef29a9d98a676bf3a457a1de466e2d8a00372b4a
dhanush3727 <dhanushs4827@gmail.com>
2025-01-15 11:54:37 +0530

    15-01-2025

--- backend/index.js
+++ backend/index.js
@@ -41,6 +41,74 @@ app.post("/books", async (req, res) => {
   }
 });
 
+// Route for get all books
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
+    return res.status(200).json({
+      data: books,
+    });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).send({ message: err.message });
+  }
+});
+
+// Route for get a book by id
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+    return res.status(200).json(book);
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).send({ msg: err.message });
+  }
+});
+
+// Route for update a book
+app.put("/books/:id", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Send all required fields : title, author, publish year",
+      });
+    }
+
+    const { id } = req.params;
+    const result = await Book.findByIdAndUpdate(id, req.body);
+    if (!result) {
+      return res.status(404).send({
+        msg: "Book not found",
+      });
+    }
+
+    return res.status(200).send({ msg: "Book Updated Successfully" });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).send({ msg: err.message });
+  }
+});
+
+// Route for delete a book
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Book.findByIdAndDelete(id);
+
+    if (!result) {
+      return res.status(404).send({
+        msg: "Book not found",
+      });
+    }
+
+    return res.status(200).send({ msg: "Book Deleted Successfully" });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).send({ msg: err.message });
+  }
+});
+
 // Connect mongoDB
 mongoose
   .connect(mongoDBURL)
