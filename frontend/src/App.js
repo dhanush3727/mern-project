4c2423e0feee5a1a4d5c20531492d7a19ca26433
dhanush3727 <dhanushs4827@gmail.com>
2025-01-16 16:18:30 +0530

    16-01-2025

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateBook from "./pages/CreateBook";
+import EditBook from "./pages/EditBook";
+import ShowBook from "./pages/ShowBook";
+import DeleteBook from "./pages/DeleteBook";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Route>
+        <Routes path="/" element={<Home />} />
+        <Routes path="/books/create" element={<CreateBook />} />
+        <Routes path="/books/detail/:id" element={<ShowBook />} />
+        <Routes path="/books/edit/:id" element={<EditBook />} />
+        <Routes path="/books/delete/:id" element={<DeleteBook />} />
+      </Route>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
